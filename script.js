edd264ca88b4002ebc16690a7697ec40b5596463
Harry Steele <harrysteele@Harrys-Air.lan>
2022-08-31 09:54:18 -0400

    finished mobile state

---
+++ script.js
@@ -0,0 +1,16 @@
+const socials = document.querySelector(".socials");
+const socialContainer = document.querySelector(".social-media")
+const body = document.querySelector("body");
+const author = document.querySelector(".author");
+const iconButton = document.querySelector(".icon-btn");
+
+socials.addEventListener("click", function() {
+    socialContainer.classList.remove("hidden");
+    author.classList.add("hidden");
+})
+
+iconButton.addEventListener("click", function() {
+    console.log("clicked");
+    socialContainer.classList.add("hidden");
+    author.classList.remove("hidden");
+})
